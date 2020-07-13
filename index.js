19e9fc4cb2a3f4e33deea7792bfaa3fc1460bc63
Thad Cole <thadjcole@gmail.com>
2020-07-13 14:59:39 -0400

    start index.js. add gereateMarkdown.js module

---
+++ index.js
@@ -0,0 +1,31 @@
+// require inquirer
+const inquirer = require("inquirer");
+
+// array of questions for user
+const questions = [
+  // project title
+  {},
+  // description
+  {},
+  // installation instructions
+  {},
+  // usage
+  {},
+  // license
+  {},
+  // contributing
+  {},
+  // tests
+  {},
+  // questions
+  {},
+];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
